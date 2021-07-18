65561f57d8172b8632deae3173343154b854753c
otoniel19 <otonielcostaoliveira3@gmail.com>
2021-07-18 13:52:05 -0300

    update

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 var obj = document.querySelector("div.img")
+var tmp = document.getElementById('seg')
 
 var i = 1
 
-var imagens = []
+var img = []
+img[0] = new Image()
 
-imagens[0] = new Image()
-imagens[0].src = "assets/res/img"+i+".jpeg"
-imagens[0].setAttribute("id", "img")
+setInterval(function() {
+  i += 1
+  if(i > 3) {
+    i = 1
+  }
+   load()
+},10000)
 
-obj.appendChild(imagens[0])
+function load() {
+img[0].src = "assets/res/img"+i+".jpeg"
+img[0].setAttribute("id", "img")
+obj.appendChild(img[0])
+}
+load()
 
+var seg = 10
+
+setInterval(function() {
+  seg--
+   if(seg <= 0) {
+     seg = 10
+   }
+   tmp.innerHTML = seg
+},1000)
